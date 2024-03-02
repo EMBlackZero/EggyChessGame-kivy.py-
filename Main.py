93c3157ce783c4ea1ed683f881961443a30039dc
ศรัณยพงศ์ เอี่ยมอนงค์ <saranyapong272547@gmail.com>
2024-03-02 22:47:06 +0700

    Change x(text) to x(Class)

--- Main.py
+++ Main.py
@@ -1,13 +1,29 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.relativelayout import RelativeLayout
+
+class Item(Label):
+    def __init__(self, namee, **kwargs):
+        super().__init__(**kwargs)
+        self.name = namee
+        self.font_size = 10
+        self.point = 1
+        self.s = 3 ; self.m = 3 ; self.l = 2  # Quantity of each size
+        self.allsize = {'S':self.s,'M':self.m,'L':self.l}
+
 
 class TicTacToe(GridLayout):
     def __init__(self, **kwargs):
-        super(TicTacToe, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.cols = 3
         self.buttons = []
-        self.current_player = 'X'
+        self.X = Item("x")
+        self.O = Item("O")
         for _ in range(3):
             row = []
             for _ in range(3):
@@ -15,16 +31,16 @@ class TicTacToe(GridLayout):
                 row.append(button)
                 self.add_widget(button)
             self.buttons.append(row)
+        self.character = self.X   # Turn Player
+        self.winner = None
 
     def on_button_press(self, instance):
         if instance.text == '':
-            instance.text = self.current_player
+            instance.text = self.character.name
             if self.check_winner():
-                self.ids.status.text = f'Player {self.current_player} wins!'
-            elif self.check_draw():
-                self.ids.status.text = 'Draw!'
+                self.show_popup(f"{self.character.name} wins!")
             else:
-                self.current_player = 'O' if self.current_player == 'X' else 'X'
+                self.character = self.O if self.character == self.X else self.X
 
     def check_winner(self):
         for i in range(3):
@@ -40,13 +56,12 @@ class TicTacToe(GridLayout):
         if self.buttons[0][2].text == self.buttons[1][1].text == self.buttons[2][0].text != '':
             return True
         return False
+    
+    def show_popup(self, text): # Popup if you win
+        popup = Popup(title='Game Over', content=BoxLayout(orientation='vertical'), size_hint=(None, None), size=(400, 200))
+        popup.content.add_widget(Button(text=text, size_hint=(None, None), size=(200, 50), pos_hint={'center_x': 0.5, 'center_y': 0.5}, on_press=popup.dismiss))
+        popup.open()
 
-    def check_draw(self):
-        for row in self.buttons:
-            for button in row:
-                if button.text == '':
-                    return False
-        return True
 
 class TicTacToeApp(App):
     def build(self):
